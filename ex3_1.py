489ed412d30799409dff23b7fdb40c579edfd15f
khoubaib <zombie.1255x@gmail.com>
2023-05-11 18:29:36 +0100

    easy

---
+++ ex3_1.py
@@ -0,0 +1,11 @@
+# Dans cette vidéo, nous allons écrire un programme qui demande à l'utilisateur de taper la largeur et la longueur d'un rectangle et qui en affiche le périmètre et la surface.
+# le perimetre = المحيط
+
+largeur = float(input("Donner largeur de la rectangle :  "))
+longueur = float(input("Donner longueur de la rectangle :  "))
+
+surface = largeur * longueur
+perimetre = (largeur + longueur)*2
+
+print("le surface =  %.3f"%surface )
+print("le perimetre = ",perimetre)
