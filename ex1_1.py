630511f5a19fb3e79a20361fe5c2287e1d8fca6f
khoubaib <zombie.1255x@gmail.com>
2023-05-11 17:58:48 +0100

    easy tak tak

---
+++ ex1_1.py
@@ -0,0 +1,5 @@
+# Dans cette vidéo, nous allons écrire un programme qui demande le nom et l'âge d’un étudiant à l'université et afficher "Bonjour ..., tu as ... ans et bienvenue à l'université" en remplaçant les ... par, respectivement le nom et l'âge.
+
+nom = input("donner ton nom  :  ")
+age = int(input("donner ton age  :  "))
+print("Bonjour",nom,"tu as",age,"ans et bienvenue a l'universite <33")
