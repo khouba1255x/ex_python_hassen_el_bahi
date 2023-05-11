86529235c4e15d7fa7d7934e3b38b45438267412
khoubaib <zombie.1255x@gmail.com>
2023-05-11 18:05:06 +0100

    easy

---
+++ ex2_1.py
@@ -0,0 +1,6 @@
+# Dans cette vidéo, nous allons écrire un programme qui demande l'année de naissance d'une personne, puis il calcule et affiche l'âge de la personne. 
+
+date_naissance = int(input("Donner l'annee de naissance :  "))
+date_cette_annee = int(input("Combien coûte l'année en cours :  "))
+
+print("tan age est",date_cette_annee - date_naissance,"annee")
