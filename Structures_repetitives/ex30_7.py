d4bda85dfbe99ea7c35d03b1ea3069dd00f18e3c
khoubaib <zombie.1255x@gmail.com>
2023-05-14 13:36:17 +0100

    version 7 le correction

---
+++ Structures_repetitives/ex30_7.py
@@ -0,0 +1,11 @@
+# il isle7 ba3d 6 versions
+# khhhhhhhhhhhhhhhdfshfkjdhfjdhfhhhhhhhhhhhhhhhhhhhhhhhhhhhhh malla code w malla houwwa 
+
+n = int(input("Veuillez saisir un nombre :  "))
+s = 1
+j = 1
+for i in range (n) :
+    s += j**2
+    j = j+2
+
+print("La somme des nombre impaires est : ", s)
